5f76d7f336ef1e3474dcf40f6b786d9344fd948e
imacrazyguylol <github@imacrazyguy.xyz>
2024-12-12 22:16:16 -0500

    I AM NOT A FAILURE

--- day12_2.java
+++ day12_2.java
@@ -1,18 +1,20 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Vector;
-import java.util.Arrays;
 
 class day12_2 extends Extra {
-    public static final int size = 4;
+    public static final int size = 140;
     public static Plant[][] grid = new Plant[size][size];
     public static Set<Region> regions = new HashSet<>();
 
     public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new FileReader(new File("inputs/day12test")));
+        BufferedReader in = new BufferedReader(new FileReader(new File("inputs/day12")));
         String line;
 
         int i = 0;
@@ -29,7 +31,7 @@ class day12_2 extends Extra {
                 boolean uniquePlant = true;
 
                 for (Region r : regions) {
-                    if (r.plants.contains(grid[i][j])) {
+                    if (r.visitedPlants.contains(grid[i][j])) {
                         uniquePlant = false;
                         break;
                     }
@@ -54,7 +56,9 @@ class day12_2 extends Extra {
     }
 
     static class Plant {
-        int sides = 0;
+        int corners = 0;
+        Vector<Plant> neighbors = new Vector<>();
+        Plant[] adjacent;
 
         int I;
         int J;
@@ -66,87 +70,64 @@ class day12_2 extends Extra {
             this.type = type;
         }
 
-        // every plant adds itself to the parent's list
-        // sideDirs: U = 0, R = 1, D = 2, L = 3
-        public void searchAdjacent(Region parent, boolean[] sideDirs) {
-            boolean searchU = false;
-            boolean searchR = false;
-            boolean searchD = false;
-            boolean searchL = false;
-            // look up
-            if (I >= 1 && this.type == grid[I - 1][J].type) { // there is no side
-                if (sideDirs[0]) {
-                    sideDirs[0] = false;
-                }
+        public void generateAdjacent() {
+            // U = 0, R = 1, D = 2, L = 3
+            // null is an edge
+            this.adjacent = new Plant[] {
+                    I >= 1 && grid[I - 1][J].type == this.type ? grid[I - 1][J] : null,
+                    J < size - 1 && grid[I][J + 1].type == this.type ? grid[I][J + 1] : null,
+                    I < size - 1 && grid[I + 1][J].type == this.type ? grid[I + 1][J] : null,
+                    J >= 1 && grid[I][J - 1].type == this.type ? grid[I][J - 1] : null
+            };
 
-                if (!parent.plants.contains(grid[I - 1][J])) {
-                    parent.plants.add(grid[I - 1][J]);
-                    searchU = true;
+            for (Plant p : adjacent) {
+                if (p != null) {
+                    neighbors.add(p);
                 }
-            } else if (!sideDirs[0]) { // there is a side
-                sides++;
-                sideDirs[0] = true;
             }
-
-            // look right
-            if (J < size - 1 && this.type == grid[I][J + 1].type) {
-                if (sideDirs[1]) {
-                    sideDirs[1] = false;
-                }
-
-                if (!parent.plants.contains(grid[I][J + 1])) {
-                    parent.plants.add(grid[I][J + 1]);
-                    searchR = true;
-                }
-            } else if (!sideDirs[1]) { // there is a side
-                sides++;
-                sideDirs[1] = true;
-            }
-
-            // look down
-            if (I < size - 1 && this.type == grid[I + 1][J].type) {
-                if (sideDirs[2]) {
-                    sideDirs[2] = false;
-                }
-
-                if (!parent.plants.contains(grid[I + 1][J])) {
-                    parent.plants.add(grid[I + 1][J]);
-                    searchD = true;
-                }
-            } else if (!sideDirs[2]) { // there is a side
-                sides++;
-                sideDirs[2] = true;
-            }
-
-            // look left
-            if (J >= 1 && this.type == grid[I][J - 1].type) {
-                if (sideDirs[3]) {
-                    sideDirs[3] = false;
-                }
-
-                if (!parent.plants.contains(grid[I][J - 1])) {
-                    parent.plants.add(grid[I][J - 1]);
-                    searchL = true;
-                }
-            } else if (!sideDirs[3]) { // there is a side
-                sides++;
-                sideDirs[3] = true;
-            }
-
-            // start recursions last so that sideDirs is updated for all of them
-            if (searchR)
-                grid[I][J + 1].searchAdjacent(parent, sideDirs); // right
-            if (searchD)
-                grid[I + 1][J].searchAdjacent(parent, sideDirs); // down
-            if (searchU)
-                grid[I - 1][J].searchAdjacent(parent, sideDirs); // recurse up
-            if (searchL)
-                grid[I][J - 1].searchAdjacent(parent, sideDirs); // left
         }
 
-        public Plant setSides(int val) {
-            this.sides = val;
-            return this;
+        // checks null adjacent tiles (null means either edge of map or edge of region, aka different type) for corners
+        public void searchAdjacent() {
+            if (adjacent[1] != null)
+                adjacent[1].generateAdjacent();
+            if (adjacent[3] != null)
+                adjacent[3].generateAdjacent();
+
+            // Check up direction first
+            if (adjacent[0] == null) { // is an edge
+                if (adjacent[1] == null) // up right outer corner
+                    corners++;
+                if (adjacent[3] == null) // up left outer corner
+                    corners++;
+            } else { // not an edge; check internal corners
+                adjacent[0].generateAdjacent();
+                if (adjacent[1] != null &&
+                        (adjacent[1].adjacent[0] == null && adjacent[0].adjacent[1] == null)) // up right internal
+                                                                                              // corner
+                    corners++;
+                if (adjacent[3] != null &&
+                        (adjacent[3].adjacent[0] == null && adjacent[0].adjacent[3] == null)) // up left internal corner
+                    corners++;
+            }
+
+            // check down now
+            if (adjacent[2] == null) { // is an edge
+                if (adjacent[1] == null) // down right outer corner
+                    corners++;
+                if (adjacent[3] == null) // down left outer corner
+                    corners++;
+            } else { // not an edge; check internal corners
+                adjacent[2].generateAdjacent();
+                if (adjacent[1] != null &&
+                        (adjacent[1].adjacent[2] == null && adjacent[2].adjacent[1] == null)) // down right internal
+                                                                                              // corner
+                    corners++;
+                if (adjacent[3] != null && adjacent[3].type == this.type &&
+                        (adjacent[3].adjacent[2] == null && adjacent[2].adjacent[3] == null)) // down left internal
+                                                                                              // corner
+                    corners++;
+            }
         }
 
         public boolean equals(Plant other) {
@@ -154,34 +135,54 @@ class day12_2 extends Extra {
         }
 
         public String toString() {
-            return this.type + " " + sides;
+            // return String.format("(%s @ [%d, %d] | sides: %d)", type, I, J, sides);
+            return type + " " + corners;
         }
     }
 
     static class Region {
-        Vector<Plant> plants = new Vector<>();
+        Vector<Plant> visitedPlants = new Vector<>();
+        private Plant startingPlant;
         private int area = 0;
-        private int sides = 0;
+        private int corners = 0;
         int price;
 
         public Region(Plant start) {
-            plants.add(start);
+            startingPlant = start;
+            startingPlant.generateAdjacent();
         }
 
+        // BFS search
         public void generate() {
-            plants.get(0).searchAdjacent(this, new boolean[] { false, false, false, false });
+            Queue<Plant> q = new ArrayDeque<>();
+            q.add(startingPlant);
 
-            this.area = plants.size();
-            for (Plant p : plants) {
-                this.sides += p.sides;
+            Plant current;
+            while (!q.isEmpty()) {
+                current = q.remove();
+                // updates the sides for this current plant and sets all of its neighbors to
+                // have the same initial sides
+                current.searchAdjacent();
+
+                // add all the valid neighbors to the queue
+                q.addAll(current.neighbors);
+
+                // remove all potential duplicated (so long as my .equals works)
+                visitedPlants.add(current);
+                q.removeAll(visitedPlants);
             }
 
-            this.price = area * sides;
+            this.area = visitedPlants.size();
+            for (Plant p : visitedPlants) {
+                this.corners += p.corners;
+            }
+
+            this.price = area * corners;
         }
 
         public boolean equals(Region other) {
-            for (Plant p : other.plants) {
-                for (Plant t : this.plants) {
+            for (Plant p : other.visitedPlants) {
+                for (Plant t : this.visitedPlants) {
                     if (t.equals(p)) {
                         return true;
                     }
@@ -191,7 +192,7 @@ class day12_2 extends Extra {
         }
 
         public String toString() {
-            return plants.toString();
+            return visitedPlants.toString();
         }
     }
 }
